a524df795d5273125d22cdda2b6bd0b9bf7b9bb8
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-10-16 22:55:15 -0400

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Timur on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
