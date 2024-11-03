59903bae42828995de7f769bcdad30c0a07b56de
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-11-03 00:02:03 -0400

    Added design in appetizer List VIew (HStack, VStack)

--- MenuApp/AppetizerListView.swift
+++ MenuApp/AppetizerListView.swift
@@ -10,9 +10,27 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationView {
-            List(MockData.appetizers, id: \.id) { appetizer in
-                Text(appetizer.name)
+            List(MockData.appetizers) { appetizer in
+                HStack {
+                    Image("asian-flank-steak")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 90)
+                        .cornerRadius(8)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(appetizer.name)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        
+                        Text("$\(appetizer.price, specifier: "%.2f")")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                    }
+                    .padding()
+                }
             }
+            .navigationTitle("Appetizers")
         }
     }
 }
