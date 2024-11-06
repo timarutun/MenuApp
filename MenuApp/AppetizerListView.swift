0749b9cf79a12bd7a7d2d53ff04bf6665e84a5d0
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-11-05 23:45:25 -0500

    Minor refactor. Created appetizerListCell

--- MenuApp/AppetizerListView.swift
+++ MenuApp/AppetizerListView.swift
@@ -11,24 +11,7 @@ struct AppetizerListView: View {
     var body: some View {
         NavigationView {
             List(MockData.appetizers) { appetizer in
-                HStack {
-                    Image("asian-flank-steak")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 120, height: 90)
-                        .cornerRadius(8)
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text(appetizer.name)
-                            .font(.title2)
-                            .fontWeight(.medium)
-                        
-                        Text("$\(appetizer.price, specifier: "%.2f")")
-                            .foregroundStyle(.secondary)
-                            .fontWeight(.semibold)
-                    }
-                    .padding()
-                }
+                AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("Appetizers")
         }
@@ -38,3 +21,5 @@ struct AppetizerListView: View {
 #Preview {
     AppetizerListView()
 }
+
+
