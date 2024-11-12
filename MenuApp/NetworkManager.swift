c0028c75f9ca8834f5f8f7c04b443e439e3fb3d9
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-11-11 23:41:52 -0500

    Started setting up the NetworkManager

---
+++ MenuApp/NetworkManager.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkManager.swift
+//  MenuApp
+//
+//  Created by Timur on 11/11/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
+    static let appetizerURL = baseURL + "appetizers"
+    
+    private init() {}
+    
+    func getAppetizers(complited: @escaping (Result<[Appetizer], Error>) -> Void) {
+        
+    }
+}
