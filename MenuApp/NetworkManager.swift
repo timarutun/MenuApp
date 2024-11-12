88ce61aaafc5d5952c46c1f1ec03c4271b388324
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-11-11 23:55:45 -0500

    Done with enum errors in NetworkManager

--- MenuApp/NetworkManager.swift
+++ MenuApp/NetworkManager.swift
@@ -12,11 +12,32 @@ final class NetworkManager {
     static let shared = NetworkManager()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
-    static let appetizerURL = baseURL + "appetizers"
+    private let appetizerURL = baseURL + "appetizers"
     
     private init() {}
     
     func getAppetizers(complited: @escaping (Result<[Appetizer], APError>) -> Void) {
+        guard let url = URL(string: appetizerURL) else {
+            complited(.failure(.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let error = error else {
+                complited(.failure(.unableToComplite))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                complited(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                complited(.failure(.invalidData))
+                return
+            }
+        }
         
     }
 }
