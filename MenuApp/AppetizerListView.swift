a6704b857c7b1e0bc43b95562e6be08aa7952cd5
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-11-20 00:33:58 -0500

    Created AppetizerListViewModel for Network Call

--- MenuApp/AppetizerListView.swift
+++ MenuApp/AppetizerListView.swift
@@ -9,33 +9,21 @@ import SwiftUI
 
 struct AppetizerListView: View {
     
-    @State private var appetizers: [Appetizer] = []
+    @StateObject var viewModel = AppetizerListViewModel()
+    
     
     var body: some View {
         NavigationView {
-            List(appetizers) { appetizer in
+            List(viewModel.appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("Appetizers")
         }
         .onAppear {
-            getAppetizers()
+            viewModel.getAppetizers()
         }
     }
-    
-    func getAppetizers() {
-        NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async{
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-        }
-        
-    }
+
 }
 
 #Preview {
---
+++ MenuApp/AppetizerListViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  AppetizerListVIewModel.swift
+//  MenuApp
+//
+//  Created by Timur on 11/20/24.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async{
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+}
