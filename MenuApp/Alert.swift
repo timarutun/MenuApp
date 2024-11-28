683fb5574b5b7fe4bc77d99585ce9853e89ec1fa
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-11-27 23:53:15 -0500

    Fimnished AlertContext

--- MenuApp/Alert.swift
+++ MenuApp/Alert.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
-    
+    static let invalidData = AlertItem(title: Text("Server Error"),
+                                       message: Text("The data received from server was invalid"),
+                                       dismissButton: .default(Text("OK")))
+    static let invalidResponse = AlertItem(title: Text("Server Error"),
+                                           message: Text("Invalid respone"),
+                                           dismissButton: .default(Text("OK")))
+    static let invalidURL = AlertItem(title: Text("Server Error"),
+                                      message: Text("Invalid URL"),
+                                      dismissButton: .default(Text("OK")))
+    static let unableToComplite = AlertItem(title: Text("Server Error"),
+                                            message: Text("Unable to complite your request at this time"),
+                                            dismissButton: .default(Text("OK")))
 }
